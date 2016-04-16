ad7e235e5b648ae545f97bd7d35068b1b6a2935a
Ryan Hamblin <ryanhca@gmail.com>
2016-04-16 09:45:40 -0600

    uncommented some code

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const cors = require('cors');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
-// const User = require('./models/users');
+const User = require('./models/users');
 const app = express();
 const port = 3333;
 
